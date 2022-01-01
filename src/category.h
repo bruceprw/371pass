1aa2d60113334d8bf6a2c3aa0ebc249778baa9a2
Martin Porcheron <martin@porcheron.uk>
2022-01-01 15:04:15 +0000

    Add student number to file headers

--- src/category.h
+++ src/category.h
@@ -2,8 +2,7 @@
 // CSC371 Advanced Object Oriented Programming (2021/22)
 // Department of Computer Science, Swansea University
 //
-// Author: Martin Porcheron
-//         m.a.w.porcheron@swansea.ac.uk
+// Author: <STUDENT NUMBER>
 //
 // Canvas: https://canvas.swansea.ac.uk/courses/24793
 // -----------------------------------------------------
